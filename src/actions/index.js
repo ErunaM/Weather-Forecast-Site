103491d612df41cc210c18219d21504c8e34d6b0
ErunaM <DeceptiveSmurf@gmail.com>
2017-12-20 14:20:39 +1300

    API requests working, TODO: using the API data

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_KEY = 'e0ee89713db9a48d15aebe240bbe03d6';
+
+const rootURL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+
+//npm install axios
+
+export function fetchWeather(city) {
+
+  const url = `${rootURL}&q=${city},nz`;
+  const request = axios.get(url);
+
+  console.log('Request',request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+
+
+  };
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.onInputChange = this.onInputChange.bind(this); // binds the components this to the function
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.state = {term: ''};
+  }
+
+  //if passing call backs around and using context e.g "this" always bind it
+  // in constructor
+  onInputChange(event) {
+
+    this.setState({ term: event.target.value});
+  }
+  onFormSubmit(event){
+    event.preventDefault();
+    //We need to go fetch the Weather data
+    console.log(this.state.term);
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''}); // clears the search bar after use
+  }
   render(){
+
      // bootstrap documentation
     return (
-      <form className="input-group">
-        <input />
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a 5 day forcast in your favorite citys!"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+         />
         <span className="input-group-btn">
           <button  type="submit" className="btn btn-secondary">
             Search
@@ -15,3 +44,10 @@ export default class SearchBar extends Component{
     );
   }
 }
+
+function mapDispatchToProps(dispatch){
+
+  return bindActionCreators({ fetchWeather },dispatch); //gives us access to this.props.fetchWeather
+}
+//null because we dont care about the state here
+export default connect(null,mapDispatchToProps)(SearchBar);
